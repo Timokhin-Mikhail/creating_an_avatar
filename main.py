63065084bb70e4cda2a7afc7a0f27d891c288edf
Mihail Timohin <mishka.tim171@gmail.com>
2023-12-26 12:42:37 +0300

    delete photo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import string
 import random
 import os
@@ -12,7 +13,7 @@ from PIL import Image
 import numpy as np
 
 
-def work_with_cam():  # (local_dir: str = None):
+def work_with_cam(max_img_heigh, max_img_wight):  # (local_dir: str = None):
     # SET THE COUNTDOWN TIMER for simplicity we set it to 3 We can also take this as input
     TIMER = int(5)
 
@@ -51,15 +52,28 @@ def work_with_cam():  # (local_dir: str = None):
                     8, cv2.LINE_AA)
         cv2.imshow('cam', img)
         cv2.waitKey(2000)
+        height, width = img.shape[:2]
 
-        unic_name = (''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-                     + datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".png")
+        if height > max_img_heigh or width > max_img_wight:
+            scale_ratio_h, scale_ratio_w = max_img_heigh/height, max_img_wight/width
+            if scale_ratio_h < scale_ratio_w:
+                scale_ratio = scale_ratio_h
+            else:
+                scale_ratio = scale_ratio_w
+            # #calculate the 50 percent of original dimensions
+            width = int(width * scale_ratio)
+            height = int(height * scale_ratio)
+            # # dsize
+            dsize = (width, height)  # resize image
+            img = cv2.resize(img, dsize)
+
+
         # if local_dir:
         #     file_path = os.path.join(local_dir + unic_name)
         #     os.chdir(local_dir)
         # else:
         #     file_path = os.path.join(os.getcwd() + unic_name)
-        height, width = img.shape[:2]
+
         # cv2.imwrite(unic_name, img)
         break
 
@@ -71,8 +85,8 @@ def work_with_cam():  # (local_dir: str = None):
     return img, height, width
 
 
-def get_background_api(prompt, negative_prompt="",sampler_name="DPM++ 2M Karras", steps=20, cfg_scale=7,
-                       denoising_strength =0, width=512, height=512):
+def get_background_api(prompt, negative_prompt="", sampler_name="DPM++ 2M Karras", steps=20, cfg_scale=7.0,
+                       denoising_strength=0.0, width=512, height=512):
     url = "http://127.0.0.1:7860"
 
     payload = {
@@ -93,15 +107,15 @@ def get_background_api(prompt, negative_prompt="",sampler_name="DPM++ 2M Karras"
 
 
 
-def refactor_person_with_background_api(prompt, img_like_str, negative_prompt="", sampler_name="DPM++ 2M Karras",
-                                        steps=20, cfg_scale=7, denoising_strength=0.4, width=512, height=512):
+def refactor_person_with_background_api(prompt, img_b64, file_name, restore_faces=True, negative_prompt="", sampler_name="DPM++ 2M Karras",
+                                        steps=20, cfg_scale=7.0, denoising_strength=0.4, width=512, height=512):
     url = "http://127.0.0.1:7860"
 
     payload = {
         "prompt": prompt,
         "negative_prompt": negative_prompt,
         "init_images": [
-                img_like_str
+                img_b64
               ],
         "sampler_name": sampler_name,
         "steps": steps,
@@ -109,14 +123,15 @@ def refactor_person_with_background_api(prompt, img_like_str, negative_prompt=""
         "width": width,
         "height": height,
         "denoising_strength": denoising_strength,
+        "restore_faces": restore_faces
     }
+
+
     response = requests.post(url=f'{url}/sdapi/v1/img2img', json=payload)
-
     r = response.json()
-
-
+    print(r)
     image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
-    image.save('output1.png')
+    image.save("ref_" + file_name)
 
 
 
@@ -125,13 +140,20 @@ def getting_person_without_background(cam_img):
     return output
 
 
-def superimposing_person_background(b64_background, array_person):
+def superimposing_person_background(b64_background, array_person, local_dir=""):
 
     img1 = Image.open(io.BytesIO(base64.b64decode(b64_background)))
     img2 = Image.fromarray(array_person)
     img1.paste(img2, (0, 0), mask=img2)
+    unic_name = (''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+                 + datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".png")
     # Saving the image
-    img1.save('pers_ready5.png')
+    img1.save(unic_name)
+    im_file = io.BytesIO()
+    img1.save(im_file, format="PNG")
+    im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
+    im_b64 = base64.b64encode(im_bytes)
+    return im_b64, unic_name
 
 
 
@@ -139,17 +161,24 @@ def superimposing_person_background(b64_background, array_person):
 prompt = ("goblin's face, made of ancient weathered rock, tall grass, moss, trees, roots, half-timbered houses and "
           "stone staircases on the rock, winding upwards, against the sky with clouds, hyperealistic, surrealism, "
           "complex concept art, atmospheric, cinematic, very attractive and fabulous")
+prompt2 = "London,  the road between the houses, RAW photo, high quality,film grain, bokeh, professional"
 # get_background_api(prompt, width=640, height=480)
 # my_image = cv2.imread(r"C:\Users\Komp\Desktop\Clear_work_with_cam\pers.jpg")
 # print(my_image.shape)
 
 
-def full_work_with_photo(prompt: str, cfg_scale="7", denoising_strength="0.4"):
+def full_work_with_photo(prompt: str, local_dir: str = "", cfg_scale: str = "7", denoising_strength: str ="0.7",
+                         max_heigh=1024, max_wight=1024):
     cfg_scale, denoising_strength = float(cfg_scale), float(denoising_strength)
-    cam_img, height_img, width_img = work_with_cam()
+    cam_img, height_img, width_img = work_with_cam(max_heigh, max_wight)
     j_person = getting_person_without_background(cam_img)
     img_b64_background = get_background_api(prompt=prompt, cfg_scale=cfg_scale, width=width_img, height=height_img)
-    superimposing_person_background(img_b64_background, j_person)
+
+    img_pers_with_back, name = superimposing_person_background(img_b64_background, j_person, local_dir)
+    refactor_person_with_background_api(prompt, img_b64_background, name, cfg_scale=cfg_scale, width=width_img,
+                                        height=height_img)
+
+
 
 # full_work_with_photo(prompt)
 # get_background_api(prompt)
@@ -175,6 +204,46 @@ def full_work_with_photo(prompt: str, cfg_scale="7", denoising_strength="0.4"):
 #     print(im_bytes)
 
 
+
+# input = cv2.imread("so.jpg")
+# scale_percent = 50
+# #calculate the 50 percent of original dimensions
+# width = int(input.shape[1] * scale_percent / 100)
+# height = int(input.shape[0] * scale_percent / 100)
+# # dsize
+# dsize = (width, height) # resize image
+# output = cv2.resize(input, dsize)
+# cv2.imwrite('os3.png', output)
+# output = remove(output, alpha_matting_erode_size=15, alpha_matting_background_threshold=100)
+# cv2.imwrite("so2.png", output)
+
+
+
+# src = cv2.imread("so2.png")
+# scale_percent = 50
+# #calculate the 50 percent of original dimensions
+# width = int(src.shape[1] * scale_percent / 100)
+# height = int(src.shape[0] * scale_percent / 100)
+# # dsize
+# dsize = (width, height) # resize image
+# output = cv2.resize(src, dsize)
+# cv2.imwrite('os3.png',output)
+
+# img1 = Image.open('so5.png')
+# im_file = io.BytesIO()
+# img1.save(im_file, format="PNG")
+# im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
+# im_b64 = str(base64.b64encode(im_bytes).decode())
+#
+#
+# refactor_person_with_background_api(prompt2, im_b64, "so9.png", restore_faces=True, width=728, height=984)
+
+# img1 = Image.open('02.png')
+# img1.paste(img2, (0, 0), mask=img2)
+#
+#     # Saving the image
+# img1.save('so6.png')
+print(os.path.join('df', 'w.png'))
 
 
 
